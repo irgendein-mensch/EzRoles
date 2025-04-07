00ec3c760f2f925e79877059c625ebcb6bbf1970
irgendein-mensch <menschkein00@gmail.com>
2025-04-07 22:15:58 +0200

    Add main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+import discord
+import os
+from dotenv import load_dotenv
+from utils.logger import get_logger
+
+logger = get_logger("EzRoles")
+
+load_dotenv()
+
+bot = discord.Bot(
+    intents=discord.Intents.all(),
+    debug_guilds=[1053821548663939072],
+    activity=discord.CustomActivity("/info | EzRoles.xyz")
+)
+
+@bot.event
+async def on_ready():
+    logger.info(f"Bot is ready. Logged in as {bot.user.name} (ID: {bot.user.id})")
+    print(f"""
+------------------ Bot started ------------------
+Name         : {bot.user.name}
+ID           : {bot.user.id}
+Servers      : {len(bot.guilds)}
+Cogs loaded  : {len(os.listdir('cogs'))}
+-------------------------------------------------
+""")
+
+if __name__ == "__main__":
+    for filename in os.listdir("cogs"):
+        if filename.endswith(".py"):
+            try:
+                bot.load_extension(f"cogs.{filename[:-3]}")
+                logger.info(f"Cog loaded: {filename}")
+            except Exception as e:
+                logger.error(f"Error when loading Cog {filename}: {e}")
+
+    try:
+        bot.run(os.getenv("TOKEN"))
+    except Exception as e:
+        logger.critical(f"Error starting the bot: {e}")
